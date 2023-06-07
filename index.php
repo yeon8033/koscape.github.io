244929e15f42366a0f12eef2870ce0df33470ee8
yeon8033 <35754573+yeon8033@users.noreply.github.com>
2023-06-07 14:30:25 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<div class="title">
+			코스케이프 예약
+		</div>
+		<div class="control">
+			<div class="wrap">
+				<a href="/koscape/login.html" class="active">로그인</a>
+				<a href="/koscape/signup.html">회원가입</a>
+			</div>
+			
+		</div>
+		<div class="btn">
+			<input id="user_id" type="text" name="user_id" placeholder="사번">
+		</div>
+		<div class="btn">
+			<input id="pw" type="password" name="pw" placeholder="비밀번호">
+		</div>
+		<div class="btn">
+			<input type="submit" class="submit signin_ok" value="로그인">
+		</div>
+	</body>
+</html>
